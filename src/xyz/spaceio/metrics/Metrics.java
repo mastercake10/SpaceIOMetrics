911a0fcc5a6ae394158325566124ee75a0d77881
mastercake10 <dev@spaceio.de>
2023-06-18 06:00:02 +0200

    cleanup

--- src/xyz/spaceio/metrics/Metrics.java
+++ src/xyz/spaceio/metrics/Metrics.java
@@ -1,21 +1,16 @@
 package xyz.spaceio.metrics;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import com.google.gson.Gson;
+import org.bukkit.plugin.Plugin;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import org.bukkit.plugin.Plugin;
-
-import com.google.gson.Gson;
+import java.util.stream.Stream;
 
 
 /*
@@ -25,63 +20,66 @@ import com.google.gson.Gson;
  */
 
 public class Metrics {
-	private Plugin pl;
+	private Plugin plugin;
 	private final Gson gson = new Gson();
 	
 	private String URL = "https://spaceio.xyz/update/%s";
 	private final String VERSION = "0.08";
 	private int REFRESH_INTERVAL = 600000;
 	
-	public Metrics(Plugin pl){
-		this.pl = pl;
-		
+	public Metrics(Plugin plugin){
+		this.plugin = plugin;
+
 		// check if Metrics are disabled (checks if file "disablemetrics" is added to the plugins's folder
-		try {
-			Files.list(pl.getDataFolder().getParentFile().toPath()).filter(Files::isRegularFile).forEach(v -> {
-				if(v.getFileName().toString().equalsIgnoreCase("disablemetrics")){
-					return;
-				}
-			});
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		try (Stream<Path> pathStream = Files.list(plugin.getDataFolder().getParentFile().toPath())) {
+			boolean disabledMetrics = pathStream.filter(Files::isRegularFile)
+					.anyMatch(file -> file.getFileName().toString().equalsIgnoreCase("disablemetrics"));
+
+			if (disabledMetrics) {
+				return;
+			}
+		} catch (IOException ignored) {
+			return;
 		}
 
-		URL = String.format(URL, pl.getName());
+		this.URL = String.format(this.URL, plugin.getName());
 		
 		// fetching refresh interval first
-		pl.getServer().getScheduler().runTaskLaterAsynchronously(pl, () -> {
+		plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, () -> {
 			String dataJson = collectData();
 			try{
-				REFRESH_INTERVAL = sendData(dataJson);
-			}catch(Exception e){}
+				this.REFRESH_INTERVAL = sendData(dataJson);
+			} catch(Exception ignored){}
 		}, 20L * 5);
 		
 		// executing repeating task, our main metrics updater
-		pl.getServer().getScheduler().runTaskTimerAsynchronously(pl, () -> {
+		plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin, () -> {
 			String dataJson = collectData();
 			try{
 				sendData(dataJson);
 			}catch(Exception e){}
 			
-		}, 20L * (REFRESH_INTERVAL / 1000), 20L * (REFRESH_INTERVAL / 1000));
+		}, 20L * (this.REFRESH_INTERVAL / 1000), 20L * (this.REFRESH_INTERVAL / 1000));
 	}
 	private String collectData() {
 		Data data = new Data();
 		
 		// collect plugin list
-		for(Plugin plug : pl.getServer().getPluginManager().getPlugins()) data.plugs.put(plug.getName(), plug.getDescription().getVersion());
-		
+		for(Plugin plug : plugin.getServer().getPluginManager().getPlugins()) {
+			data.plugs.put(plug.getName(), plug.getDescription().getVersion());
+		}
+
 		// fetch online players
-		data.onlinePlayers = pl.getServer().getOnlinePlayers().size();
+		data.onlinePlayers = plugin.getServer().getOnlinePlayers().size();
 		
 		// server version
 		data.serverVersion = getVersion();
 		
 		// plugin version
-		data.pluginVersion = pl.getDescription().getVersion();
+		data.pluginVersion = plugin.getDescription().getVersion();
 		
 		// plugin author
-		data.pluginAuthors = pl.getDescription().getAuthors();
+		data.pluginAuthors = plugin.getDescription().getAuthors();
 		
 		// core count
 		data.coreCnt = Runtime.getRuntime().availableProcessors();
@@ -90,19 +88,18 @@ public class Metrics {
 		data.javaRuntime = System.getProperty("java.runtime.version");
 		
 		// online mode
-		data.onlineMode = pl.getServer().getOnlineMode();
+		data.onlineMode = plugin.getServer().getOnlineMode();
 
 		// software information
 		data.osName = System.getProperty("os.name");
 		data.osArch = System.getProperty("os.arch");
 		data.osVersion = System.getProperty("os.version");
-		
-		String executableName = new java.io.File(Metrics.class.getProtectionDomain()
+
+		data.executableName = new java.io.File(Metrics.class.getProtectionDomain()
 				  .getCodeSource()
 				  .getLocation()
 				  .getPath())
 				.getName();
-		data.executableName = executableName;
 		
 		if(data.osName.equals("Linux")){
 			data.linuxDistro = getDistro();
@@ -127,7 +124,7 @@ public class Metrics {
 		return Integer.parseInt(con.getHeaderField("interval-millis"));
 	}
 	private String getVersion(){
-        String packageName = pl.getServer().getClass().getPackage().getName();
+        String packageName = plugin.getServer().getClass().getPackage().getName();
         return  packageName.substring(packageName.lastIndexOf('.') + 1);
 	}
 	// method source: http://www.jcgonzalez.com/linux-get-distro-from-java-examples
@@ -135,21 +132,21 @@ public class Metrics {
 		
 		// lists all the files ending with -release in the etc folder
         File dir = new File("/etc/");
-        File fileList[] = new File[0];
+        File[] fileList = new File[0];
         if(dir.exists()){
-            fileList =  dir.listFiles(new FilenameFilter() {
-                public boolean accept(File dir, String filename) {
-                    return filename.endsWith("-release");
-                }
-            });
+            fileList =  dir.listFiles((dir1, filename) -> filename.endsWith("-release"));
         }
         try {
+			if(fileList == null) {
+				return "unknown";
+			}
+
 	        // looks for the version file (not all linux distros)
 	        File fileVersion = new File("/proc/version");
 	        if(fileVersion.exists() && fileList.length > 0){
 	            fileList = Arrays.copyOf(fileList,fileList.length+1);
 	            fileList[fileList.length-1] = fileVersion;
-	        }    
+	        }
 	        
 	        // prints first version-related file
 	        for (File f : fileList) {
@@ -160,7 +157,7 @@ public class Metrics {
 	                }
 	                br.close();
 	        }
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         	// Exception is thrown when something went wrong while obtaining the distribution name.
         }
 		return "unknown";    
